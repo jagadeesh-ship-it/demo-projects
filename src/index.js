39dff7d5ed91556892e5160847a897d8cb10df02
jagadeesh-ship-it <jagadeeshreddy9944@gmail.com>
2025-03-07 14:14:08 +0530

    changed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Signup from './components.jsx/Signup';
+
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Signup/>
+  
   </React.StrictMode>
 );
 
